697a2edac52eb0176cee315a508d5e8a5871d657
dhruv-kandpal <56139983+dhruv-kandpal@users.noreply.github.com>
2020-10-13 01:25:14 +0530

    Update wordlist.sh

--- wordlist.sh
+++ wordlist.sh
@@ -5,13 +5,12 @@ path="/"
 read -p "Enter any known path of parent domain from where you want to skim words, or press enter for default (/)..." path
 echo "[+]Finding paths on ${pdomain}....."
 gau -subs ${pdomain} > ${pdomain}.urls
-cat ${pdomain}.urls | unfurl -u paths| sed 's#/#\n#g' | sort -u > ${pdomain}.paths
+cat ${pdomain}.urls | unfurl -u paths | sed 's#/#\n#g' | sort -u | grep -v [[:digit:]] > ${pdomain}.paths
 echo "[+]Finding parameters on ${pdomain}....."
-cat ${pdomain}.urls | unfurl -u keys > ${pdomain}.parameters 
+cat ${pdomain}.urls | unfurl -u keys | sed 's#/#\n#g' | sort -u | grep -v [[:digit:]] > ${pdomain}.parameters 
 echo "[+] Optimizing Results!"
 printf "\n"
 cat ${pdomain}.paths | grep -Ev "\.html$|==$|\.png$|\.jpg$|\.css$|\.gif$|\.pdf$|\.js$|\.jpeg$|\.tif$|\.tiff$|\.ttf$|\.woff$|\.woff2$|\.ico$|\.svg$|\.txt$" | sort -u > paths ; cat paths > ${pdomain}.paths ; rm paths
-cat ${pdomain}.parameters | sort -u > params ; cat params > ${pdomain}.parameters ; rm params
 echo "[+] Finding generic words..."
 wget https://${pdomain}/${path} --no-check-certificate -O ${pdomain}.html
 cat ${pdomain}.html | tok | tr '[[:upper:]]' '[[:lower:]]' | sort -u  | anew ${pdomain}.words 
