f10766e486bfa1f240f85f8fbd58aa3d2eb829ae
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2020-11-27 18:47:09 +0530

    Update wordlistWeaver.sh

--- wordlistWeaver.sh
+++ wordlistWeaver.sh
@@ -18,13 +18,14 @@ pflag=0 	#flag to input paths-list
 function usage()
 {
 	echo -e "\n${PINK}[+] Usage:\n\t./wordlistWeaver.sh  <target-domain name>"
-	echo -e " ${ORANGE}-p : ${GREY}to specify a specific path of the target domain to grab generic words. (Default path:\"/\")" 
-	echo -e " ${ORANGE}${BOLD}\n 	  Eg: ${GREEN}${BOLD}./wordlistWeaver.sh -p /some_path_on_example.com/ example.com\n"
-	echo -e " ${ORANGE}-P : ${GREY}to specify a list of paths of the target domain to grab generic words. (Default path:\"/\")" 
-	echo -e " ${ORANGE}${BOLD}\n 	  Eg: ${GREEN}${BOLD}./wordlistWeaver.sh -P exampls.com_paths.txt example.com\n"
-	echo -e "${ORANGE} -U : ${GREY}to specify a file containing URLs of the target to avoid fetching them from gau and waybackurls."
+	echo -e " ${ORANGE}-p : ${GREY}to specify a specific path of the target domain OR a URL (path) of the target domain/subdomain to grab generic words. (Default path:\"/\")" 
+	echo -e " ${ORANGE}${BOLD}\n 	  Eg: ${GREEN}${BOLD}./wordlistWeaver.sh -p /some/specific/path/ example.com -> will fetch words from https://example.com/some/specific/path/"
+	echo -e " ${ORANGE}${BOLD} 	  Eg: ${GREEN}${BOLD}./wordlistWeaver.sh -p https://xyz.example.com/some/path/on/xyz example.com -> will fetch words from that subdomain URL\n"
+	echo -e " ${ORANGE}-P : ${GREY}to specify a list of paths (URLs) of the target domain/sub-domain to grab generic-words. (Default path:\"/\" i.e https://target-domain/)" 
+	echo -e " ${ORANGE}${BOLD}\n 	  Eg: ${GREEN}${BOLD}./wordlistWeaver.sh -P example.com_paths.txt example.com\n"
+	echo -e "${ORANGE} -U : ${GREY}to specify a file containing URLs of the target to avoid fetching them."
 	echo -e " ${ORANGE}${BOLD}\n 	  Eg: ${GREEN}${BOLD}./wordlistWeaver.sh -U example.com_urls.txt -p /some/specific/path/ example.com\n"
-	echo -e "${ORANGE} -S : ${GREY}to specify a file containing subdomains of the target to fetch their paths from gau and waybackurls."
+	echo -e "${ORANGE} -S : ${GREY}to specify a file containing subdomains of the target to fetch their URLs explicitely."
 	echo -e " ${ORANGE}${BOLD}\n 	  Eg: ${GREEN}${BOLD}./wordlistWeaver.sh -S example.com_subdomains.txt -p /some/specific/path/ example.com\n"
 	echo -e "${ORANGE} -h : ${GREY}to display usage."
 	echo -e "${OFFWHITE}\n[+] IMPORTANT: Flags must be written before the target-domain name"
@@ -57,9 +58,9 @@ if [[ $# -ne 1 ]] ; then
 	exit 1
 else
  	echo -e "${GOLD}\n[+] The wizards are weaving your target-specific wordlists! :))) \n"
- 	if [[ $urlflag -eq 1 ]] ; then
+ 	if [[ $urlflag -eq 1 && $subflag -eq 0 ]] ; then
  		cat $urlfile | anew -q ${1}.urls
- 	elif [[ $subflag -eq 1 ]] ; then
+ 	elif [[ $subflag -eq 1 && $urlflag -eq 0 ]] ; then
  		echo -e "${GREY}[+] Gathering URLs for *.${1}"
 	 	gau -subs ${1} | anew -q ${1}.urls & waybackurls ${1} | anew -q ${1}.urls
 	 	wait
@@ -72,6 +73,9 @@ else
 		printf "\n"
  	elif [[ $urlflag -eq 1 && $subflag -eq 1 ]] ; then
  		cat $urlfile | anew -q ${1}.urls
+ 		echo -e "${GREY}[+] Gathering URLs for *.${1}"
+	 	gau -subs ${1} | anew -q ${1}.urls & waybackurls ${1} | anew -q ${1}.urls
+	 	wait
  		for line in $(cat $subfile) ; do
 			echo -e "${GREY}[+] Gathering URLs for $line..."
 			waybackurls $line | anew -q ${1}.urls & gau $line | anew -q ${1}.urls
@@ -89,34 +93,47 @@ else
  	cat ${1}.urls | unfurl -u paths| sed 's#/#\n#g' | sort -u | grep ... | grep -Ev "%|\-\-|[[:lower:]]+-[[:lower:]]+-[[:lower:]]+|^[[:digit:]]+|^-|^_|^-[[:digit:]]|^[[:lower:]]+[[:upper:]]|.*,.*|[[:upper:]]+[[:lower:]]+[[:upper:]]+|_|[[:upper:]]+[[:digit:]]+|[[:lower:]]+[[:digit:]][[:digit:]]+[[:lower:]]*|[[:upper:]]+[[:digit:]][[:digit:]]+[[:lower:]]*|[[:alpha:]]+-[[:alpha:]]+-|^[[:digit:]]+|\.html$|==$|\.png$|\.jpg$|\.css$|\.gif$|\.pdf$|\.js$|\.jpeg$|\.tif$|\.tiff$|\.ttf$|\.woff$|\.woff2$|\.ico$|\.svg$|\.txt$" | anew -q ${1}.paths-wordlist.txt
  	echo -e "${GREEN}[+] $(cat ${1}.paths-wordlist.txt| wc -l) Paths obtained! :D"
  	echo -e "${ORANGE}\n[+] Finding parameters on ${1}....."
- 	cat ${1}.urls | unfurl -u keys | grep -Ev "%|\-\-|[[:lower:]]+-[[:lower:]]+-[[:lower:]]+|^[[:digit:]]+|^-|^_|^-[[:digit:]]|^[[:lower:]]+[[:upper:]]|.*,.*|[[:upper:]]+[[:lower:]]+[[:upper:]]+|_|[[:upper:]]+[[:digit:]]+|[[:lower:]]+[[:digit:]][[:digit:]]+[[:lower:]]*|[[:upper:]]+[[:digit:]][[:digit:]]+[[:lower:]]*|[[:alpha:]]+-[[:alpha:]]+-|^[[:digit:]]+|\.html$|==$|\.png$|\.jpg$|\.css$|\.gif$|\.pdf$|\.js$|\.jpeg$|\.tif$|\.tiff$|\.ttf$|\.woff$|\.woff2$|\.ico$|\.svg$|\.txt$" | anew -q ${1}.parameters-wordlist.txt
+ 	cat ${1}.urls | unfurl -u keys | anew -q ${1}.parameters-wordlist.txt
  	echo -e "${GREEN}[+] $(cat ${1}.parameters-wordlist.txt| wc -l) Parameters obtained! :D" 
  	printf "\n"
 	if [[ $pflag -eq 1 ]] ; then
 		for line in $(cat $pathfile) ; 
 		do
-			echo -e "${ORANGE}[+] Finding generic words from: https://${1}$line "
-			wget https://${1}$line --wait=3 -U "Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36" --no-http-keep-alive --no-check-certificate --tries=1 -O ${1}.html &> /dev/null & sleep 7 ; 
+			echo -e "${ORANGE}[+] Fetching generic words from: $line "
+			wget $line --wait=3 -U "Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36" --no-http-keep-alive --no-check-certificate --tries=1 -O ${1}.html &> /dev/null & sleep 7 ; 
 			cat ${1}.html >> fetched_paths
 		done
 		cat fetched_paths > ${1}.html ; rm fetched_paths
 	else
-		echo -e "${ORANGE}[+] Finding generic words from: https://${1}${path} "
-		wget https://${1}${path} --wait=3 -U "Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36" --no-http-keep-alive --no-check-certificate --tries=1 -O ${1}.html &> /dev/null & sleep 8 ; 
+		echo $path | grep ${1} | grep http > /dev/null
+		if [[ $? -eq 0 ]] ; then
+			echo -e "${ORANGE}[+] Fetching generic words from: ${path} "
+			wget ${path} --wait=3 -U "Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36" --no-http-keep-alive --no-check-certificate --tries=1 -O ${1}.html &> /dev/null & sleep 7 ; 
+		else
+			echo -e "${ORANGE}[+] Fetching generic words from: https://${1}${path} "
+			wget https://${1}${path} --wait=3 -U "Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36" --no-http-keep-alive --no-check-certificate --tries=1 -O ${1}.html &> /dev/null & sleep 8 ; 
+		fi
 	fi
 	if [[ -s ${1}.html ]] ; then
 		cat ${1}.html | tok | tr '[[:upper:]]' '[[:lower:]]' | grep -Ev "\-\-|[[:lower:]]+-[[:lower:]]+-[[:lower:]]+|^[[:digit:]]+|^-|^_|^-[[:digit:]]|^x|^[[:lower:]]+[[:upper:]]|[[:upper:]]+[[:lower:]]+[[:upper:]]+|_|[[:upper:]]+[[:digit:]]+|[[:lower:]]+[[:digit:]]+[[:lower:]]*|[[:upper:]]+[[:digit:]][[:digit:]]+[[:lower:]]*|[[:alpha:]]+-[[:alpha:]]+-|^[[:digit:]]+" | grep .. | sort -u  | anew -q ${1}-words 
-		#comm -13 <path_to_rfc_words>./rfc-words ${1}-words | anew -q ${1}.generic-wordlist.txt
+		comm -13 /home/hack3rwiz/Downloads/hacks/rfc-words ${1}-words | anew -q ${1}.generic-wordlist.txt
 		echo -e "${GREEN}[+] $(cat ${1}.generic-wordlist.txt | wc -l) Generic-words generated! :D"
 		cat ${1}.generic-wordlist.txt | anew -q ${1}.paths-wordlist.txt
 		rm ${1}-words 
 		rm ${1}.html
 	else
-		echo -e "${RED}[-] The specified path could not be fetched from the domain. Please try again. :("
-		disown
-		killall -9 wget &> /dev/null
-		rm ${1}.html
-		rm ${1}.urls
+		echo $path | grep ${1} | grep http > /dev/null
+		if [[ $? -eq 0 ]] ; then
+			echo -e "${RED}[-] The specified path: ${path} could not be fetched. Please try with a different path or try again. :("
+			disown
+			killall -9 wget &> /dev/null
+			rm ${1}.html
+		else
+			echo -e "${RED}[-] The specified path: https://${1}${path} could not be fetched. Please try with a different path or try again. :("
+			disown
+			killall -9 wget &> /dev/null
+			rm ${1}.html
+		fi
 	fi
 	echo -e "${GOLD}\n[+] Thank you for using Wordlist-Weaver! :D"
 fi
