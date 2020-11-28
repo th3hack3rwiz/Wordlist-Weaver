f69da0be96e11e28b1146d14f596799e37b7087a
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2020-11-29 02:19:50 +0530

    To fulfil requirements, run setup.sh!

---
+++ setup.sh
@@ -0,0 +1,28 @@
+GREEN='\e[38;5;149m'
+GOLD='\e[38;5;226m'
+git clone https://github.com/tomnomnom/hacks 
+cd hacks/tok
+echo -e "${GREEN}[+] Building tok"
+go build
+cp tok $(echo $GOPATH)/bin/tok 
+cd ../unfurl
+echo -e "${GREEN}[+] Building unfurl"
+go build
+cp unfurl $(echo $GOPATH)/bin/unfurl
+cd ../anew
+echo -e "${GREEN}[+] Building anew"
+go build
+cp anew $(echo $GOPATH)/bin/anew
+cd ../waybackurls
+echo -e "${GREEN}[+] Building waybackurls"
+go build
+cp waybackurls $(echo $GOPATH)/bin/waybackurls
+echo -e "${GREEN}[+] Installing gau"
+GO111MODULE=on go get -u -v github.com/lc/gau
+cd ../../
+curl https://tools.ietf.org/html/rfc1866 -o rfc.html
+cat rfc.html | tok | tr '[[:upper:]]' '[[:lower:]]' | sort -u > rfc-words
+rm rfc.html
+printf "\n"
+echo -e "${GOLD}$(pwd)/rfc-words"
+echo -e "${GREEN}[+] Now add the above path ^ of rfc-words' to wordlistWeaver.sh: Line number -> 131"
